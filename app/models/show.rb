2e5ff1f573637f6c78e1966be2bcd7da7bcc8619
George  Pianka <gpianka1@gmail.com>
2018-10-15 14:43:15 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,23 +1,23 @@
 class Show < ActiveRecord::Base
    def self.highest_rating
     Show.maximum(:rating)
-  end
+   end
    def self.most_popular_show
     Show.find_by(:rating)
-  end
+   end
    def self.lowest_rating
     Show.minimum(:rating)
-  end
+   end
    def self.least_popular_show
     Show.find_by_rating(lowest_rating)
-  end
+   end
    def self.ratings_sum
     Show.sum("rating")
-  end
+   end
    def self.popular_shows
     Show.where("rating > 5")
-  end
+   end
    def self.shows_by_alphabetical_order
     Show.order(:name)
-  end
+   end
 end
